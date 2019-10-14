22402476517055b617a687940874771b521319d3
Hardelele <i.lapshina@mail.ru>
2019-10-15 02:02:55 +0300

    Edit Triangle.java Add new methods to work with triangles.

--- src/main/abstracts/Triangle.java
+++ src/main/abstracts/Triangle.java
@@ -4,9 +4,26 @@ import main.objects.Node;
 
 public interface Triangle {
 
-    public Node[] nodes = new Node[3];
+    Node[] nodes = new Node[3];
 
     void calculateS();
     double getS();
+
+    default double ribLength(Node firstNode, Node secondNode) {
+
+        double lengthX = Math.abs(firstNode.getX() - secondNode.getX());
+        double lengthY = Math.abs(firstNode.getY() - secondNode.getY());
+        double diagonalLength = getDiagonalLength(lengthX,lengthY);
+
+        return diagonalLength;
+    }
+
+    default double getSumOfSqrLengths(double rib1, double rib2) {
+        return Math.pow(rib1,2) + Math.pow(rib2,2);
+    }
+
+    default double getDiagonalLength(double rib1, double rib2) {
+        return Math.sqrt(getSumOfSqrLengths(rib1,rib2));
+    }
 
 }
