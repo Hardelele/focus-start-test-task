6209ac2aff69fc048960f99ab3dae85792a085c1
Hardelele <i.lapshina@mail.ru>
2019-10-15 02:01:10 +0300

    Edit MainExecutor.java Refactor code: add new methods and make readable names.

--- src/main/algorithms/MainExecutor.java
+++ src/main/algorithms/MainExecutor.java
@@ -14,21 +14,41 @@ public class MainExecutor extends FileReceiver {
 
     public static int counter = 0;
 
-    private static ArrayList<IsoscelesTriangle> triangles = new ArrayList<>();
+    public static ArrayList<IsoscelesTriangle> triangles = new ArrayList<>();
 
-    public static void getAndSaveTriangleNodes() {
-        StringTokenizer tokenizer = new StringTokenizer(FocusStartApplication.cornetString," ");
-        double tokenX,tokenY;
+    public static void saveTriangleProps() {
+        tokenizer = new StringTokenizer(FocusStartApplication.cornetString," ");
+        double cornetX,cornetY;
 
         while(tokenizer.hasMoreTokens()) {
-            tokenX = Double.parseDouble(tokenizer.nextToken());
-            tokenY = Double.parseDouble(tokenizer.nextToken());
-            triangles.add(new IsoscelesTriangle());
-            triangles.get(counter).setTriangleNodesCoords(tokenX,tokenY);
-            triangles.get(counter).calculateS();
+
+            cornetX = Double.parseDouble(tokenizer.nextToken());
+            cornetY = Double.parseDouble(tokenizer.nextToken());
+
+            createNewTriangle();
+            saveNodes(cornetX,cornetY);
+            calculateS();
+            calculateAndSaveRibs();
+
             counter++;
         }
 
+    }
+
+    private static void createNewTriangle() {
+        triangles.add(new IsoscelesTriangle());
+    }
+
+    private static void saveNodes(double x, double y) {
+        triangles.get(counter).setTriangleNodesCoords(x,y);
+    }
+
+    private static void calculateS() {
+        triangles.get(counter).calculateS();
+    }
+
+    private static void calculateAndSaveRibs() {
+        triangles.get(counter).calculateAndSaveLengthOfRibs();
     }
 
 }
