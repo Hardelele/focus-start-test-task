68de889d257f2e53975612c45e67f6607e876212
Hardelele <i.lapshina@mail.ru>
2019-10-16 19:40:37 +0300

    Rename Node.java to Point.java

---
+++ src/main/Point.java
@@ -0,0 +1,32 @@
+
+package main;
+
+public class Point {
+
+    private double x, y;
+
+    public Point() {
+    }
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+}
