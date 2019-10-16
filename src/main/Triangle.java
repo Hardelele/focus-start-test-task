df7726e436c7a1c3576c4b1f05a54d4318093a26
Hardelele <i.lapshina@mail.ru>
2019-10-16 19:55:58 +0300

    Start implementation Triangle.java - Create final variable countOfPoints = 3 - Add boolean variable isNull - Add array of 3 points - Constructor Triangle(), for null triangles, that call method setPoints0 - Method setPoints0 set variable isNull as true, also set all points[] as 0.

--- src/main/Triangle.java
+++ src/main/Triangle.java
@@ -2,4 +2,21 @@ package main;
 
 public class Triangle {
 
+    private final static int countOfPoints = 3;
+
+    private boolean isNull;
+
+    private Point[] points = new Point[countOfPoints];
+
+    Triangle() {
+        setPoints0();
+    }
+
+    public void setPoints0() {
+        for (int counter = 0; counter < countOfPoints; counter++) {
+            isNull = true;
+            points[counter].setX(0);
+            points[counter].setY(0);
+        }
+    }
 }
