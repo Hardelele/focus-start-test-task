cc7e4a03e067dbb9e93799cedf089b3dbf6ab8f8
Hardelele <i.lapshina@mail.ru>
2019-10-17 17:52:34 +0300

    Implemented new methods and constructor in Triangle class - Add double S variable - Method calculateS is empty in this moment - Method getS return S - @Override method toString() of class Object, this one collect array of points in string and return it - Rename method setPoints0 to setPointsAs0 - setPoints() get String param and parse array of points from this one - Constructor with String param only use method setPoints() in this moment

--- src/main/MainExecutor.java
+++ src/main/MainExecutor.java
@@ -4,6 +4,10 @@ public class MainExecutor {
 
     private static String[] args;
 
+    private static String cornetString = null;
+
+    private static Triangle maxSTriangle = null;
+
     /**
      * Method run() only execute methods, that implemented in MainExecutor.java
      */
@@ -25,14 +29,35 @@ public class MainExecutor {
 
     /**
      * Method setIOFiles have to execute 2 methods:
-     * FileReader.setInputFile(args[0]);
-     * FileWriter.setOutputFile(args[1]);
+     * FileReader.setInputFile(args[0]) and FileWriter.setOutputFile(args[1])
      * It's used in method run(), to make that one more readable.
      */
 
     private static void setIOFiles() {
-        FileReader.setInputFile(args[0]);
-        FileWriter.setOutputFile(args[1]);
+        HFileReader.setInputFile(args[0]);
+        HFileWriter.setOutputFile(args[1]);
+    }
+
+    /**
+     * Method setDesiredTriangleEmpty call constructor Triangle() without params,
+     * that set all points as 0, set S = 0 and set flag isNull = true.
+     */
+
+    private static void setMaxSTriangleAsEmpty() {
+        maxSTriangle = new Triangle();
+    }
+
+    /**
+     * Method compareWithMaxSTriangle compare maxSTriangle with thisTriangle
+     * and if second one are more, set maxSTriangle as thisTriangle
+     *
+     * @param thisTriangle it's triangle that we need to compare with maxSTriangle
+     */
+
+    private static void compareWithMaxSTriangle(Triangle thisTriangle) {
+        if (thisTriangle.getS() > maxSTriangle.getS()) {
+            maxSTriangle = thisTriangle;
+        }
     }
 
 }
