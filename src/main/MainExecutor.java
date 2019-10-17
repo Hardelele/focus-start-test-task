cbaa6a469c54e60b694fcf48368911bc71d2284b
Hardelele <i.lapshina@mail.ru>
2019-10-17 21:41:55 +0300

    Edit MainExecutor, add 4 new methods - Add method saveResult(), that write result in output file - Add method searchMaxSTriangle(), someone help me to fix it, there is infinity loop :'( - Add method setCornetString() - Add method checkIOFiles() - Has used this four in method run()

--- src/main/MainExecutor.java
+++ src/main/MainExecutor.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.HashSet;
+
 public class MainExecutor {
 
     private static String[] args;
@@ -14,6 +16,10 @@ public class MainExecutor {
 
     public static void run() {
         setIOFiles();
+        checkIOFiles();
+        setMaxSTriangleAsEmpty();
+        searchMaxSTriangle();
+        saveResult();
     }
 
     /**
@@ -38,6 +44,11 @@ public class MainExecutor {
         HFileWriter.setOutputFile(args[1]);
     }
 
+    private static void checkIOFiles() {
+        HFileReader.checkInputFile();
+        HFileWriter.checkOutputFile();
+    }
+
     /**
      * Method setDesiredTriangleEmpty call constructor Triangle() without params,
      * that set all points as 0, set S = 0 and set flag isNull = true.
@@ -58,6 +69,31 @@ public class MainExecutor {
         if (thisTriangle.getS() > maxSTriangle.getS()) {
             maxSTriangle = thisTriangle;
         }
+    }
+
+    /**
+     * Method setCornetString() get string and set it in MainExecutor.cornetString
+     *
+     * @param cornetString it is string of 6 points
+     */
+
+    private static void setCornetString(String cornetString) {
+        MainExecutor.cornetString = cornetString;
+    }
+
+    private static void searchMaxSTriangle() {
+        Triangle cornetTriangle;
+        while (HFileReader.endOfFile()){
+            setCornetString(HFileReader.readNext());
+            cornetTriangle = new Triangle(cornetString);
+            compareWithMaxSTriangle(cornetTriangle);
+            cornetString = "";
+        }
+    }
+
+    private static void saveResult() {
+        String result = maxSTriangle.toString();
+        HFileWriter.write(result);
     }
 
 }
