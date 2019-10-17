39122860e4912065bf23c61a0e90956878b30a6e
Hardelele <i.lapshina@mail.ru>
2019-10-17 21:38:04 +0300

    Has ended of implementation of HFileWriter.java - Add methods: write() and checkOutputFile() - Add variables FileWriter fileWriter, BufferedWriter bufferedWriter and set outputFile = null on start of program

--- src/main/HFileWriter.java
+++ src/main/HFileWriter.java
@@ -1,13 +1,54 @@
 package main;
 
-import java.io.File;
+import java.io.*;
 
 public class HFileWriter {
 
-    private static File outputFile;
+    private static FileWriter fileWriter = null;
+
+    private static BufferedWriter bufferedWriter = null;
+
+    private static File outputFile = null;
 
     public static void setOutputFile(String ref) {
         HFileWriter.outputFile = new File(ref);
+    }
+
+    /**
+     * Method checkOutputFile() check existence of
+     * output File and if it isn't, create that one.
+     */
+
+    public static void checkOutputFile() {
+
+        if(!outputFile.exists()) {
+
+            try {
+                outputFile.createNewFile();
+            } catch (IOException e) {
+                ExceptionsPrinter.printIOException();
+            }
+
+        }
+
+    }
+
+    /**
+     * Method write() save writableString as result in outputFile
+     */
+
+    public static void write(String writableString) {
+
+        try {
+            fileWriter = new FileWriter(outputFile);
+            bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(writableString);
+            bufferedWriter.flush();
+            bufferedWriter.close();
+        } catch (IOException e) {
+            ExceptionsPrinter.printIOException();
+        }
+
     }
 
 }
